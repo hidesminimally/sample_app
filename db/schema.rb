9890fbc997a5aaa303a39ade3a41f3007c2cc342
Shimmy Li <danielmxli@gmail.com>
2015-07-25 22:05:09 -0700

    Fix DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726002635) do
+ActiveRecord::Schema.define(version: 20150720004509) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
